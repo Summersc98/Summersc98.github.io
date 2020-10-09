d14d5b7f34a454c3b80912ce3782fcf065dbedfa
Chris Summers <Summersc98@users.noreply.github.com>
2020-10-09 17:07:02 -0400

    i dont know what im doing

--- public/lab_5/scripts/main.js
+++ public/lab_5/scripts/main.js
@@ -9,10 +9,14 @@
 const myHeading = document.querySelector('h1');
 myHeading.textContent = 'the js loaded properly';
 
-let btn = document.querySelector('button');
+const btn = document.querySelector('button');
+function random(number) {
+  return Math.floor(Math.random() * (number+1));
+}
 btn.onclick = function() {
-    
-};
+  const rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+  document.body.style.backgroundColor = rndCol;
+}
 
 //let button = document.querySelector('button');
 //button.style.marginRight = '10px';
