a9b1633eb71b4769fcd626bee2c3f820e5eceab0
Chris Summers <Summersc98@users.noreply.github.com>
2020-10-09 13:56:27 -0400

    Update main.js
    T

--- public/lab_5/scripts/main.js
+++ public/lab_5/scripts/main.js
@@ -1,2 +1,6 @@
 const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
+
+document.querySelector('html').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}
