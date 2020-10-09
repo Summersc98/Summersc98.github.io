f09bca8ba33935bd8b33a606d04d6cab3aa9598c
Chris Summers <Summersc98@users.noreply.github.com>
2020-10-09 16:43:15 -0400

    ahhh
    ahhhh

--- public/lab_5/scripts/main.js
+++ public/lab_5/scripts/main.js
@@ -5,10 +5,16 @@
 /* eslint-disable indent */
 /* eslint-disable prefer-const */
 /* eslint-disable no-multiple-empty-lines */
+/* eslint-disable spaced-comment */
 const myHeading = document.querySelector('h1');
 myHeading.textContent = 'the js loaded properly';
 
-let button = document.querySelector('button');
-button.style.marginRight = '10px';
+let btn = document.querySelector('button');
+btn.onclick = function() {
+    
+};
+
+//let button = document.querySelector('button');
+//button.style.marginRight = '10px';
 
 
