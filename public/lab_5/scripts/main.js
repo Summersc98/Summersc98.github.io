d7b77ab90ad9cdc337de98f6ddc466f621ad3bc3
Chris Summers <Summersc98@users.noreply.github.com>
2020-10-09 15:26:35 -0400

    Update main.js
    i added button styling

--- public/lab_5/scripts/main.js
+++ public/lab_5/scripts/main.js
@@ -1,2 +1,14 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-alert */
+/* eslint-disable comma-spacing */
+/* eslint-disable func-names */
+/* eslint-disable indent */
+/* eslint-disable prefer-const */
+/* eslint-disable no-multiple-empty-lines */
 const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+myHeading.textContent = 'the js loaded properly';
+
+let button = document.querySelector('button');
+button.style.marginRight = '10px';
+
+
