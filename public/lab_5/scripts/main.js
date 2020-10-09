ee08b41286c53db35a0a45aa69ec5a4fc0b2c236
Chris Summers <Summersc98@users.noreply.github.com>
2020-10-09 14:18:39 -0400

    both
    removed that annoying alret nonsense and made the script runable in the thing

--- public/lab_5/scripts/main.js
+++ public/lab_5/scripts/main.js
@@ -1,7 +1,2 @@
 const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
-
-document.querySelector('html').onclick = function() {
-    // eslint-disable-next-line no-alert
-    alert('Ouch! Stop poking me!');
-};
